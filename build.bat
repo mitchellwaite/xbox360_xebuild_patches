043b0effd5f05db5ced6f08a1551913830a02332
Mitchell Waite <mitchellwaite@users.noreply.github.com>
2025-12-17 17:59:44 -0500

    Add XDKBuild patches

--- build.bat
+++ build.bat
@@ -17,6 +17,12 @@ call:buildPatchSection src\KHV\17489_RGLoader khv_vfuses_trinitybb
 call:buildPatchSection src\KHV\17489_RGLoader khv_vfuses_coronabb
 call:buildPatchSection src\KHV\17489_RGLoader khv_vfuses_devkit
 
+call:buildPatchSection src\KHV\17489_XDKBuild khv_vfuses_sb
+call:buildPatchSection src\KHV\17489_XDKBuild khv_vfuses_jasperbb
+call:buildPatchSection src\KHV\17489_XDKBuild khv_vfuses_trinitybb
+call:buildPatchSection src\KHV\17489_XDKBuild khv_vfuses_coronabb
+call:buildPatchSection src\KHV\17489_XDKBuild khv_vfuses_devkit
+
 echo Done!
 
 echo.
@@ -67,18 +73,45 @@ copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_devkit.bin + src\K
 
 echo Done!
 
+echo 17489 XDKBuild...
+
+mkdir output\17489_XDKBuild
+
+REM *** For XDKBuild, the patch sets for 16mb machines are all identical as they use the same loaders and kernel
+copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_sb.bin + src\KHV\17489_XDKBuild\khv_vfuses_sb.bin output\17489_XDKBuild\patches_g2mjasper.bin
+copy output\17489_XDKBuild\patches_g2mjasper.bin output\17489_XDKBuild\patches_g2mxenon.bin
+copy output\17489_XDKBuild\patches_g2mjasper.bin output\17489_XDKBuild\patches_g2mzephyr.bin
+copy output\17489_XDKBuild\patches_g2mjasper.bin output\17489_XDKBuild\patches_g2mfalcon.bin
+copy output\17489_XDKBuild\patches_g2mjasper.bin output\17489_XDKBuild\patches_g2mtrinity.bin
+copy output\17489_XDKBuild\patches_g2mjasper.bin output\17489_XDKBuild\patches_g2mcorona.bin
+
+REM *** BB consoles all have different patch sets. C'est la vie.
+copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_bb.bin + src\KHV\17489_XDKBuild\khv_vfuses_jasperbb.bin output\17489_XDKBuild\patches_g2mjasper_flash.bin
+copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_bb.bin + src\KHV\17489_XDKBuild\khv_vfuses_trinitybb.bin output\17489_XDKBuild\patches_g2mtrinity_flash.bin
+copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_sb.bin + src\KHV\17489_XDKBuild\khv_vfuses_coronabb.bin output\17489_XDKBuild\patches_g2mcorona_flash.bin
+
+REM *** This is a bit hacky for a devkit image...
+REM *** Corona BB KHV patches are the same as SB minus the HDD redirection (i.e. fuses and patches are at 0xE0000)
+REM *** The "devkit" SD patch looks for XeLL at 0xF4000 because XeBuild can't be consistent in where the start
+REM *** of the filesystem in a small block image is. SB patches are ignored pretty much.. just here to make sure
+REM *** the sections in the patch file are correct and xeBuild doesn't freak out (plz open source xebuild)
+copy /b src\2BL\14352\sb_vfuses.bin + src\4BL\17489\sd_vfuses_devkit.bin + src\KHV\17489_XDKBuild\khv_vfuses_devkit.bin output\17489_XDKBuild\patches_devjasper.bin
+
+echo Done!
+
+
 echo.
 echo SHA256 hashes:
 echo.
-certutil -hashfile "output\17489_RGLoader\patches_g2mjasper.bin" SHA256
+certutil -hashfile "output\17489_XDKBuild\patches_g2mjasper.bin" SHA256
 echo.
-certutil -hashfile "output\17489_RGLoader\patches_g2mjasper_flash.bin" SHA256
+certutil -hashfile "output\17489_XDKBuild\patches_g2mjasper_flash.bin" SHA256
 echo.
-certutil -hashfile "output\17489_RGLoader\patches_g2mtrinity_flash.bin" SHA256
+certutil -hashfile "output\17489_XDKBuild\patches_g2mtrinity_flash.bin" SHA256
 echo.
-certutil -hashfile "output\17489_RGLoader\patches_g2mcorona_flash.bin" SHA256
+certutil -hashfile "output\17489_XDKBuild\patches_g2mcorona_flash.bin" SHA256
 echo.
-certutil -hashfile "output\17489_RGLoader\patches_devjasper.bin" SHA256
+certutil -hashfile "output\17489_XDKBuild\patches_devjasper.bin" SHA256
 echo.
 
 echo.
